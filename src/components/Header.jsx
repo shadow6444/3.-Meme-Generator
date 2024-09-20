a37bab7b41c47036facb798fb2100d327718a36b
shadow6444 <shadowgarden644@gmail.com>
2024-09-20 15:02:27 +0500

    Added Header File

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,15 @@
-export default function Header(){
-    return (
-        <h1>Hello</h1>
+import logo from "../assets/troll-face.png";
 
-    );
+export default function Header() {
+  return (
+    <nav className="nav">
+      <div className="nav-items">
+        <div className="nav-logo">
+          <img src={logo} alt="meme logo" />
+          <h1>Meme Generator</h1>
+        </div>
+        <h3>React Course - Project 3</h3>
+      </div>
+    </nav>
+  );
 }
